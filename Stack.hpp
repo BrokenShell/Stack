b99e4d47b00523f38d8d64de88fb36dc076204ee
BrokenShell <webmaster@sharpdesigndigital.com>
2020-09-24 18:11:35 -0700

    Initial commit

---
+++ Stack.hpp
@@ -0,0 +1,108 @@
+#pragma once
+
+#include <vector>
+#include <cassert>
+#include <cmath>
+
+
+namespace Containers {
+
+    template<typename T>
+    class Monoid {
+        T value;
+    public:
+        Monoid() = default;
+        explicit Monoid(T val) : value(val) {}
+
+        auto get() const {
+            return value;
+        }
+
+        template<typename UT>
+        auto set(UT&& val) {
+            value = std::forward<UT>(val);
+            return *this;
+        }
+    };
+
+    template<typename T>
+    class Monad {
+        T value;
+    public:
+        explicit Monad(T val) : value(val) {}
+
+        auto get() const {
+            return value;
+        }
+
+        template<typename UT>
+        auto set(UT&& val) const {
+            return Monad(std::forward<UT>(val));
+        }
+    };
+
+    template<typename T>
+    class Stack {
+        std::vector<T> data;
+    public:
+        Stack() = default;
+
+        template<typename UT>
+        void push(UT&& value) {
+            // Perfect Forwarding by Universal Reference &&
+            data.push_back(std::forward<UT>(value));
+        }
+
+        auto pop() -> T {
+            assert(!data.empty());
+            auto result {data.back()};
+            data.pop_back();
+            return result;
+        }
+
+        auto size() const {
+            return data.size();
+        }
+
+        auto begin() const {
+            return data.crbegin();
+        }
+
+        auto end() const {
+            return data.crend();
+        }
+    };
+
+    template<typename T>
+    class Queue {
+        std::vector<T> data;
+    public:
+        Queue() = default;
+
+        template<typename UT>
+        void push(UT&& value) {
+            // Perfect Forwarding by Universal Reference &&
+            data.insert(data.begin(), std::forward<UT>(value));
+        }
+
+        auto pop() -> T {
+            assert(!data.empty());
+            auto result {data.back()};
+            data.pop_back();
+            return result;
+        }
+
+        auto size() const {
+            return data.size();
+        }
+
+        auto begin() const {
+            return data.crbegin();
+        }
+
+        auto end() const {
+            return data.crend();
+        }
+    };
+
+} // end namespace
